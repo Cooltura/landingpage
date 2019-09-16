7a163c68d9154e5f39c41784828c291945725c02
Cooltura <33953312+Cooltura@users.noreply.github.com>
2019-09-16 10:55:36 +0200

    Add files via upload

--- src/utils/texts.js
+++ src/utils/texts.js
@@ -7,19 +7,19 @@ export const texts={
         button:'ZAREJESTRUJ SIĘ',
     },
     header:{
-        header:'Ciesz się z dodatkowych przychodów.',
-        text:'Ciesz się z dodatkowych przychodów. Ciesz się z dodatkowych przychodów i redukcji kosztów.',
+        header:'System aukcyjny dla zamkniętej grupy użytkowników.',
+        text:'Zbuduj dodatkowy kanał sprzedaży, realizuj wyprzedaże na rzecz klientów lub pracowników, pozbądź się zalegającego towaru z magazynu dzięki prostemu w obsłudze Twojemu własnemu systemowi aukcyjnemu.',
         button:'WYPRÓBUJ ZA DARMO'
     },
     slider:{
         header:'Wypróbowane przez:',
     },
     description:{
-        header1:'Ciesz się z dodatkowych przychodów.',
-        header2:'Ciesz się z dodatkowych przychodów.',
-        text1:'Ciesz się z dodatkowych przychodów.Ciesz się z dodatkowych przychodów.',
-        text2:'Ciesz się z dodatkowych przychodów.Ciesz się z dodatkowych przychodów.',
-    },
+        header1:'Podstawowe funkcjonalności.',
+        header2:'Cechy wyróżniające rozwiązanie KUPTAM.PL',
+        text1:['Przeglądanie oferowanych aukcji, wraz z funkcjami podglądu cen i czasu zakończenia aukcji w czasie rzeczywistym.','Wystawianie towaru w formie różnego typu licytacji bądź z opcją „kup teraz”.','Aukcje prywatne (nawet dla jednej firmy / osoby).','Powiadomienia e-mail.','Raportowanie sprzedaży i zakupów.','Zarządzanie użytkownikami (w tym zapraszanie nowych, nadawanie i odbieranie uprawnień).'],
+        text2:['Prostota i przejrzystość funkcjonalności.','Atrakcyjna szata graficzna.','Bardzo szybkie wystawianie przedmiotów na aukcje.','Uruchamianie w przeglądarce internetowej i aplikacji mobilnej.','Bardzo szybkie wdrożenie dla grupy docelowych użytkowników. '],
+     },
     action:{
         header:'SPRAWDŹ JAKIE TO PROSTE',
         button:'ZAŁÓŻ KONTO',
@@ -28,17 +28,18 @@ export const texts={
     },
     reference:{
         header:"NASI KLIENCI KOCHAJĄ NAS",
-        text:'przeczytaj co o nas mówią nasi klienci. KUP TAM POPROSTU DZIAŁA!',
-        textcompany:'przeczytaj co o nas mówią nasi klienci. KUP TAM POPROSTU DZIAŁA!przeczytaj co o nas mówią nasi klienci. KUP TAM POPROSTU DZIAŁA!przeczytaj co o nas mówią nasi klienci. KUP TAM POPROSTU DZIAŁA!przeczytaj co o nas mówią nasi klienci. KUP TAM POPROSTU DZIAŁA!przeczytaj co o nas mówią nasi klienci. KUP TAM POPROSTU DZIAŁA!przeczytaj co o nas mówią nasi klienci. KUP TAM POPROSTU DZIAŁA!',
+        text:'Przeczytaj co o nas mówią nasi klienci. KUP TAM POPROSTU DZIAŁA!',
+        textcompany1:'Opracowany przez INNOKREA sp. z o.o. system aukcyjny wyróżnia się praktycznością oraz dopracowaną funkcjonalnością, umożliwiającą m.in. intuicyjne i proste w obsłudze zakupy oraz łatwe wystawianie nowych przedmiotów na aukcje. Klienci FEMAX Sp. z o.o. Sp.K. pozytywnie zareagowali na nowe narzędzie. Usługi wdrożeniowe i wsparcia świadczone przez INNOKREA sp. z o.o. są wykonywane z ponadprzeciętną jakością, w sposób profesjonalny, staranny i solidny. Terminy reakcji na zgłaszane wnioski są krótkie.',
+        textcompany2:'W wyniku współpracy z firmą INNOKREA udało nam się wdrożyć nowatorskie rozwiązania i znacząco usprawnić proces sprzedaży. Na szczególne uznanie zasługuje rzetelny, kompetentny i profesjonalny zespół informatyków, który doskonale rozumie potrzeby biznesowe. Z pełnym przekonaniem możemy polecić firmę INNOKREA wszystkim podmiotom poszukującym solidnego i godnego zaufania partnera.',
     },
     adventage:{
         header:'UŻYWAJĄC KUPTAM MOŻESZ ZYSKAĆ',
-        texts:['DODATKOWE PRZYCHODY','ZDROWY MAGAZYN']
+        texts:['DODATKOWE PRZYCHODY','ZDROWY MAGAZYN','PROFESJONALNE WSPARCIE','UPORZĄDKOWANY PROCES','OSZCZĘDNOŚĆ','UCZCIWE WYPRZEDAŻE','REDUKCJA KOSZTÓW','NADZÓR SPRZEDEAŻY','ZADOWOLENIE PRACOWNIKÓW']
 
     },
     usage:{
-        header:'Ciesz się z dodatkowych przychodów',
-        text:'Ciesz się z dodatkowych przychodów i redukcji kosztów. Ciesz się z dodatkowych przychodów i redukcji kosztów.',
+        header:'Wynajem lokalu lub powierzchni',
+        text:'Załóżmy, że dysponujesz lokalem usługowym, działką lub wagonem (!) i chciałbyś otrzymać jak najlepszą cenę za wynajem tej przestrzeni... Jak zrobić to szybko i efektywnie ? Zaproś swoich oferentów w ramach systemu aukcyjnego KUPTAM.PL do licytacji! ',
         button1:'WIĘCEJ',
         button2:'SPRAWDŹ WIĘCEJ WDROŻEŃ',
     },
@@ -52,10 +53,18 @@ export const texts={
         text:'Zdaj się na nasz zespół. Kompleksowo obsłużymy cały proces wyprzedaży',
         headernr1:'WYŚLIJ SPRZĘT',
         textnr1:'do innokrea',
+        headernr2:'STWORZYMY PLATFORMĘ AUKCYJNĄ',
+        textnr2:' według Twoich wymagań',
+        headernr3:'PRZEPROWADZIMY PROCES WYPRZEDAŻY',
+        textnr3:'sprzętu',
+        headernr4:'ROZEŚLEMY ZAKUPIONY SPRZĘT',
+        textnr4:'do kupujących (na terenie Polski)',
+        headernr5:'ANALIZUJ RAPORTY',
+        textnr5:'z wyprzedaży',
     },
     footer:{
         header:['WSPARCIE','KONTAKT'],
-        text:[['Podręcznik sprzedającego','Podręcznik kupującego','FAQ','Regulamin','Polityka plików cookies'],['Innokrea Sp.z.o.o','Lęborska 3b,80-386 Gdańsk','tel.:48694487332','e-mail:info@innokrea.pl']],
+        text:[['Podręcznik sprzedającego','Podręcznik kupującego','FAQ','Regulamin','Polityka plików cookies'],['Innokrea Sp.z.o.o','Lęborska 3b,80-386 Gdańsk','tel.: +48 694 487 332','e-mail:info@innokrea.pl']],
         bottomtext:'Copyright Innokrea Sp.z o. o.. Wszystkie prawa zastrzeżone',
     }
 };
